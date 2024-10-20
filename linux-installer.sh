06aff582b7bcedf204dbbc8eeb19e0238cdd1654
Obie Hinojosa <sarushinobie@proton.me>
2024-10-19 22:58:15 -0500

    add linux-installer.sh

---
+++ linux-installer.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo "################"
+echo SCRIPT STARTING
+echo "################"
+
+#template
+#printf "${process0}"\\r;
+#zip;
+#printf "${process0} \e[32m[DONE!]\e[0m"
+
+moddir="/home/$USER/.minecraft"
+cd ${moddir}
+
+process0="Backing up existing mod folder..."
+printf "${process0}"\\r;
+zip -r -q ./mods/.mod-folder-backup.zip mods && printf "${process0} \e[32m[DONE!]\e[0m"
+echo
+sleep 1
+
+process1="Removing old mods..."
+printf "${process1}"\\r;
+sudo rm /home/$USER/.minecraft/mods/* && printf "${process1} \e[32m[DONE!]\e[0m"
+echo
+sleep 1
+
+
+
+moddir="/home/$USER/.minecraft/mods"
+cd ${moddir}
+
+
+
+process2="Downloading mod package..."
+printf "${process2}"\\r;
+sudo curl http://192.168.7.208:420/api/shares/YxMjU3N/files/e641be0e-bccd-4f3d-8a86-7dbbb8f88263 -O -J -L -s && printf "${process2} \e[32m[DONE!]\e[0m"
+echo
+sleep 1
+
+
+process3="Extracting package..."
+printf "${process3}"\\r;
+unzip -q mod-archive.zip;
+printf "${process3} \e[32m[DONE!]\e[0m"
+echo
+sleep 1
+
+#yes this is all for show it looks cool shut up
+printf "Finalizing"\\r; sleep 1; printf "Finalizing."\\r; sleep 1; printf "Finalizing.."\\r; sleep 1; printf "Finalizing..."\\r; sleep 1;printf "Finalizing... \e[32m[DONE!]\e[0m";
+echo
+sleep 1
+echo Exiting...
+sleep 4
+
+exit
