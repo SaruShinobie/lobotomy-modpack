1eb7d44926ee25fb143c6134d00bf50acd1572bd
saru <sarushinobie@proton.me>
2024-10-24 18:17:31 -0500

    i forgor

--- installer-windows.bat
+++ installer-windows.bat
@@ -12,8 +12,13 @@ echo "################"
 echo.
     @timeout /t 1 >nul 2>&1
 
+cd /d %mcroot%
+
+echo Downloading latest fabric installer...
+    curl -k "https://maven.fabricmc.net/net/fabricmc/fabric-installer/1.0.1/fabric-installer-1.0.1.exe" -o fabricinstaller.exe
+    start /WAIT "%mcroot%" fabricinstaller.exe
+
 echo Updating Mods...
-    cd /d %mcroot%
 
     curl -k "https://git.adolin.xyz/saru/lobotomy-mod-pack/src/commit/07dba1b0ab21e3a764f9e31c82fe459e6ab0b133/mods.tar.gz" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8" -H "Accept-Language: en-US,en;q=0.5" -H "Accept-Encoding: gzip, deflate, br, zstd" -H "Prefer: safe" -H "Alt-Used: git.adolin.xyz" -H "Connection: keep-alive" -H "Upgrade-Insecure-Requests: 1" -H "Sec-Fetch-Dest: document" -H "Sec-Fetch-Mode: navigate" -H "Sec-Fetch-Site: same-origin" -H "Authorization: token ***REMOVED***" -o mods.tar.gz
 
