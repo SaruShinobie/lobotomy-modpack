ba2c8ad56065527edf2a6a358458edac2e031f73
Obie Hinojosa <sarushinobie@proton.me>
2024-10-23 22:03:51 -0500

    add missing code comments

--- installer-linux.sh
+++ installer-linux.sh
@@ -12,9 +12,13 @@ echo "################"
 moddir="/home/$USER/.minecraft"
 cd ${moddir}
 
+
+
 process0="Backing up existing mod folder..."
 printf "${process0}"\\r;
 zip -r -q ./mods/.mod-folder-backup.zip mods && printf "${process0} \e[32m[DONE!]\e[0m"
+#i didnt even know `zip` as a standalone command existed.
+#this needs to be a tar.gz archive.
 echo
 sleep 1
 
@@ -34,6 +38,9 @@ cd ${moddir}
 process2="Downloading mod package..."
 printf "${process2}"\\r;
 sudo curl http://192.168.7.208:420/api/shares/YxMjU3N/files/e641be0e-bccd-4f3d-8a86-7dbbb8f88263 -O -J -L -s && printf "${process2} \e[32m[DONE!]\e[0m"
+#what the fuck?
+#how does this work?
+#fix this.
 echo
 sleep 1
 
@@ -52,4 +59,5 @@ sleep 1
 echo Exiting...
 sleep 4
 
+#god bash is so much nicer than windows CMD scripts
 exit
