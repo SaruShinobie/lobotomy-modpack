736f7eecb94df02a569504d5412e9f5f2852b07a
saru <sarushinobie@proton.me>
2024-10-27 10:46:11 -0500

    fix mods extracting to root minecraft folder

--- installer-windows.bat
+++ installer-windows.bat
@@ -21,10 +21,10 @@ echo Downloading latest fabric installer...
     start /WAIT "%mcroot%" fabricinstaller.exe
 
 
-echo Updating Mods...
+echo Updating...
     cd /d %mcroot%
 
-    curl -k "https://git.adolin.xyz/saru/lobotomy-mod-pack/raw/branch/main/mods.tar.gz" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8" -H "Accept-Language: en-US,en;q=0.5" -H "Accept-Encoding: gzip, deflate, br, zstd" -H "Prefer: safe" -H "Alt-Used: git.adolin.xyz" -H "Connection: keep-alive" -H "Upgrade-Insecure-Requests: 1" -H "Sec-Fetch-Dest: document" -H "Sec-Fetch-Mode: navigate" -H "Sec-Fetch-Site: same-origin" -H "Authorization: token ***REMOVED***" -o mods.tar.gz
+    curl -k "https://git.adolin.xyz/saru/lobotomy-mod-pack/raw/branch/main/mods.tar.gz" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8" -H "Accept-Language: en-US,en;q=0.5" -H "Accept-Encoding: gzip, deflate, br, zstd" -H "DNT: 1" -H "Sec-GPC: 1" -H "Alt-Used: git.adolin.xyz" -H "Connection: keep-alive" -H "Upgrade-Insecure-Requests: 1" -H "Sec-Fetch-Dest: document" -H "Sec-Fetch-Mode: navigate" -H "Sec-Fetch-Site: same-origin" -H "Sec-Fetch-User: ?1" -H "Priority: u=0, i" -H "TE: trailers" -H "Authorization: token ***REMOVED***" -o mods.tar.gz
 
 
     echo Compressing and backing up existing mods...
@@ -32,12 +32,12 @@ echo Updating Mods...
     @timeout /t 2 >nul 2>&1
 
     echo Deleting existing mods...
-    @RD /S /Q mods
-    @MD mods
+    rmdir /S /Q mods
+    mkdir mods
 
     @timeout /t 2 >nul 2>&1
     
-    tar -xvzf %mcroot%/mods.tar.gz -C %mcroot%
+    tar -xvzf %mcroot%/mods.tar.gz -C %modfolder%
 
 echo Sigma!
 echo All done.
